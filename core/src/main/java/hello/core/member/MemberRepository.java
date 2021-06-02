3bd254d72be2247dccf9460687d7b10dfa7364e8
YOUNGBINJEON <sh44972@gmail.com>
2021-06-03 00:59:40 +0900

    [Update] 순수JAVA를 이용한 회원 도메인 테스트

--- core/src/main/java/hello/core/member/MemberRepository.java
+++ core/src/main/java/hello/core/member/MemberRepository.java
@@ -1,5 +1,6 @@
 package hello.core.member;
 
+//회원 저장소
 public interface MemberRepository {
 
     void save(Member member);
--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -3,9 +3,11 @@ package hello.core.member;
 import java.util.HashMap;
 import java.util.Map;
 
+
+//멤버 저장소
 public class MemoryMemberRepository implements MemberRepository {
 
-    private  static Map<Long, Member> store = new HashMap<>();
+    private static Map<Long, Member> store = new HashMap<>();
 
     @Override
     public void save(Member member) {
