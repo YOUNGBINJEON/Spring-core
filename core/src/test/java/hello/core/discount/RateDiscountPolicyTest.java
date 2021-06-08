50cb1faeef6af022406e9aa3194549b1e25419c9
YOUNGBINJEON <sh44972@gmail.com>
2021-06-08 23:29:06 +0900

    [Update] 새로운 할인 정책 실패 테스트

--- core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -26,4 +26,17 @@ class RateDiscountPolicyTest {
         Assertions.assertThat(discount).isEqualTo(1000);
     }
 
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야한다")
+    void vip_x() {
+
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
 }
