16010ceb382ea669e6024bcaf22784f1bace902c
YOUNGBINJEON <sh44972@gmail.com>
2021-06-10 01:39:09 +0900

    [Update] 관심사의 분리 AppConfig

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,10 +4,13 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        MemberService memberService = new MemberServiceImpl(null);
+        OrderService orderService = new OrderServiceImpl(null, null);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
