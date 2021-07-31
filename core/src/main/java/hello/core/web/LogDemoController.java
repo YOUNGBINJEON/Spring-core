9643fe55531b3c4708a9945b8327bc261f40bf6c
YOUNGBINJEON <sh44972@gmail.com>
2021-07-31 22:40:25 +0900

    [Update] 스코프와 Provider - Thread test

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -19,14 +19,14 @@ public class LogDemoController {
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
 
         String requestURL = request.getRequestURL().toString();
         MyLogger myLogger = myLoggerProvider.getObject();
-
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("Controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
 
         return "OK";
